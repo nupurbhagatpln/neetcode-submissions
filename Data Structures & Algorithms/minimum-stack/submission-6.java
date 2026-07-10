5864f9ff872e58914ad137b5a51008d88f7ceff3
nupurbhagatpln <101352933+nupurbhagatpln@users.noreply.github.com>
2026-07-10 10:29:36 +0530

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.java
@@ -0,0 +1,42 @@
+class MinStack {
+    Stack<Long> st;
+    long min;
+
+    public MinStack() {
+        st= new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(st.isEmpty())
+        {
+            st.push(0L);
+            min=val;
+        }
+        else {
+            st.push(val-min);
+            if(val<min){
+                min=val;
+            }
+        }
+    }
+    
+    public void pop() {
+        if(st.isEmpty())    return;
+
+        long top= st.pop();
+        if(top<0){
+            min=min-top;
+        }
+    }
+    
+    public int top() {
+        long top= st.peek();
+        if(top>0)   return (int)(top+min);
+        else return (int)min;
+    }
+    
+    public int getMin() {
+        return (int )min;
+        
+    }
+}
